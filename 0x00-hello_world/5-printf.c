ace407ce57e7967acd205360318887e839fde6c3
keby24 <eklenmkeby@gmail.com>
2023-02-17 00:08:20 -0800

    printf() assignment

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description : A program that prints exactly with proper grammar,
+ *  but the outcome is a piece of art,
+ *
+ * Return: Always (0) Succes
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
